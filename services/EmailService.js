170a4fc34ff04db04515df2e1ff481d4707bf486
Puspendra Pandey <puspendra.p@foodybuddy.in>
2019-03-07 01:56:03 +0530

    first commit

--- services/EmailService.js
+++ services/EmailService.js
@@ -1,25 +1,25 @@
 var nodemailer = require('nodemailer');
-const email = 'pusp94@gmail.com';
+const email = 'styc484@gmail.com';
 
 var transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: email,
-    pass: 'Pandey@123'
+    pass: 'Styc1;1Styc'
   }
 }); 
 
 const sendMail = (message) => {
     var mailOptions = {
-        from: 'pusp94@gmail.com',
-        to: 'pusp94@gmail.com',
+        from: email,
+        to: email,
         subject: 'STYC QUERY',
         html: message
       };
       
       transporter.sendMail(mailOptions, function(error, info){
         if (error) {
-          console.log('MAIL ERROR',error);
+          console.log(error);
           throw error;
         } else {
           console.log('Email sent: ' + info.response);
